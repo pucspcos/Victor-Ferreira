6f9b412c320b02be599d08f0809dca08f788aff7
Victor Ferreira <viktor.ferreira150@gmail.com>
2016-11-04 23:17:25 -0200

    Trabalho_FINAL_mario
    Templo da lua final

---
+++ templo_da_lua/Assets/Scripts/changing_scene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class changing_scene : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+	
+}
---
+++ templo_da_lua/Assets/Scripts/cl_3.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class cl_3 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnMouseDown()
+    {
+        //print("OLA");
+        SceneManager.LoadScene("cena03");
+    }
+}
